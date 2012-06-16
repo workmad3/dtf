38e72bce2418c6eb07a97f0894cc8db576f436e7
Deryl R. Doucette <me@deryldoucette.com>
2012-06-16 18:34:28 -0400

    Added lib/config/environment.rb for environmental control. Updated .gitignore to remove config dir

---
+++ lib/config/environment.rb
@@ -0,0 +1,46 @@
+# Application wide requirements
+require 'active_record'
+require 'active_model'
+require 'active_support'
+require 'multi_json'
+require 'sqlite3'
+require 'yaml'
+require 'logger'
+
+# Set the ENV which defaults to 'development'. Also affects which database to create and connect to.
+ENV['RAILS_ENV'] ||= 'development'
+
+# This section is for development and testing. Load your testing framework(s) require's here
+case ENV['RAILS_ENV']
+when 'development', 'test'
+  require 'rspec'
+  require 'turnip'
+  require 'pry'
+  require 'pry-nav'
+  require 'pry-coolline'
+  require 'pry-doc'
+  require 'pry-stack_explorer'
+  require 'pry-exception_explorer'
+  require 'pryable'
+  require 'pry-git'
+  require 'pry-editline'
+  require 'pry-highlight'
+  require 'pry-buffers'
+  require 'pry-developer_tools'
+  require 'pry-syntax-hacks'
+  require 'fabrication'
+else
+  true
+end
+
+# Load the db config and create a connectoid. Make an ivar so its shared throughout the application
+@dbconfig = YAML::load(File.open(File.join(File.dirname(__FILE__), '../../db/config.yml')))[ENV['RAILS_ENV']]
+
+# Establish the database connection
+ActiveRecord::Base.establish_connection(@dbconfig) # Line that actually connects the db.
+
+# Load all the models
+Dir["#{File.join(File.dirname(__FILE__), '../../app/models/*.rb')}"].each do |model|
+  load "#{model}"
+end
+
