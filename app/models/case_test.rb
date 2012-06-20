d3eb72eebc6bfa36b3f0426f90cb09eeaf54734a
Deryl R. Doucette <me@deryldoucette.com>
2012-06-20 01:33:00 -0400

    Last bit of ordering on case_test

--- app/models/case_test.rb
+++ app/models/case_test.rb
@@ -4,6 +4,6 @@ class CaseTest < ActiveRecord::Base
   validates_presence_of :description, :cmd
 
   belongs_to  :analysis_case
-  has_many    :users, :through => :verification_suite
   has_one     :verification_suite, :through => :analysis_case
+  has_many    :users, :through => :verification_suite
 end
