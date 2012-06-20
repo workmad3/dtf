35d2fa216fd72861936c5a808f70d3d87ea2a445
Deryl R. Doucette <me@deryldoucette.com>
2012-06-20 00:51:21 -0400

    Fix association ordering in models

--- app/models/analysis_case.rb
+++ app/models/analysis_case.rb
@@ -3,8 +3,8 @@ class AnalysisCase < ActiveRecord::Base
   attr_accessible :name, :description
   validates_presence_of :name, :description
 
-  has_many    :users, :through => :verification_suite
   belongs_to  :verification_suite
+  has_many    :users, :through => :verification_suite
   has_many :case_tests
 
 end
--- app/models/case_test.rb
+++ app/models/case_test.rb
@@ -3,8 +3,7 @@ class CaseTest < ActiveRecord::Base
   attr_accessible :description, :cmd
   validates_presence_of :description, :cmd
 
-  has_many    :users, :through => :verification_suite
-  has_one     :verification_suite, :through => :analysis_case
   belongs_to  :analysis_case
-
+  has_one     :verification_suite, :through => :analysis_case
+  has_many    :users, :through => :verification_suite
 end
