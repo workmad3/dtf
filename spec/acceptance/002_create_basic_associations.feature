f02205a2f34016606637fcc19672e6d34a29aafe
Deryl R. Doucette <me@deryldoucette.com>
2012-07-02 06:53:35 -0400

    Added additional steps definitions and feature for testing basic model associations

--- spec/acceptance/002_create_basic_associations.feature
+++ spec/acceptance/002_create_basic_associations.feature
@@ -2,4 +2,10 @@ Feature: Verification of associations
   Background:
     Given a user
 
-  Scenario:
+  Scenario: it should own verification suites
+    When I create a verification suite
+    Then I should see my verification suite
+
+  Scenario: it should own analysis cases
+    When I create a analysis case
+    Then I should see my analysis case
--- spec/support/custom_matchers/model_steps.rb
+++ spec/support/custom_matchers/model_steps.rb
@@ -2,23 +2,35 @@ module ModelSteps
 
   step "a user" do
     @user = Fabricate(:user)
+    @user.should_not be_nil
   end
 
   step "a verification suite" do
     @vs = Fabricate(:verification_suite)
+    @vs.should_not be_nil
   end
 
   step "I should see an analysis case" do
     @ac = Fabricate(:analysis_case)
+    @ac.should_not be_nil
   end
 
   step "I should see a case test" do
     @ct = Fabricate(:case_test)
+    @ct.should_not be_nil
   end
 
   step "I should see user ownership chain via :association" do |association|
     @user.send(association).build
-    @user.send(association).nil?
+    @user.send(association).should_not be_nil
+  end
+
+  step "I create a/an :association" do |association|
+    @user.send(association.pluralize).build
+  end
+
+  step "I should see my :association" do |association|
+    @user.send(association.pluralize).should_not be_empty
   end
 
 end
