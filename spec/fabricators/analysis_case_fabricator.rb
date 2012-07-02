45125611b6049857e24e597c69779379d949ab6e
Deryl R. Doucette <me@deryldoucette.com>
2012-07-02 06:52:48 -0400

    Removed auto-fabrication of a verification suite for fabricated analysis cases as that will cause problems in testing.

--- spec/fabricators/analysis_case_fabricator.rb
+++ spec/fabricators/analysis_case_fabricator.rb
@@ -1,6 +1,5 @@
 Fabricator(:analysis_case) do
   name "DTF Analysis Case"
   description "Fabricated AC for DTF testing"
-  verification_suite { Fabricate(:verification_suite) }
   case_tests(count: 3)
 end
