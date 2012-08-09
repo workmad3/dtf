6a2ee5a7f5fae3c1a5dd6b57d8065653009ea0bb
Deryl R. Doucette <me@deryldoucette.com>
2012-08-09 16:57:03 -0400

    Implemented testing for dtf help system|modular exec system

--- spec/acceptance/0003_execute_help_switch.feature
+++ spec/acceptance/0003_execute_help_switch.feature
@@ -3,5 +3,5 @@ Feature: Display DTF help system to users upon request
     Given I have dtf installed
 
     Scenario: User should be able to request DTF command-specific help overview
-      When I type "dtf-cmd --help"
+      When I type "dtf --help"
       Then I should see help system output
--- spec/steps/feature_steps.rb
+++ spec/steps/feature_steps.rb
@@ -5,13 +5,14 @@ step "I have dtf installed" do
   end
 end
 
-# $cmd is any one of the various current dtf-<cmd> management / control scripts
+# $cmd is any one of the various current dtf cmd management / control structures
 # TODO: Rewrite to use a single dtf control script, and modular cmd|subcmd exec|help systems
 step "I type :cmd" do |cmd|
   # Change this to check programmatically that $cmd implements --help param
-  %x[#{cmd}]
+  @cmd_text = %x[bin/#{cmd}]
 end
 
 step "I should see help system output" do
   # Display actual help for specific $cmd space
+  @cmd_text.should include("help")
 end
