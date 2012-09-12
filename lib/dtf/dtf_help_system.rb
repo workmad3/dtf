409d335bcc8e8307ebad863763312d74eab18813
David Deryl Downey <me@daviddwdowney.com>
2012-09-12 09:54:47 -0400

    Rewrote option parameters in help system and main script to make the option names match the field names in the models. I did it so that the names of the hash keys matched the model variables for easier management.

--- lib/dtf/dtf_help_system.rb
+++ lib/dtf/dtf_help_system.rb
@@ -27,23 +27,24 @@ end
 @cmd_opts = case @cmd
   when "create_user"
     Trollop::options do
-      opt(:user, desc="Username for new TF user - REQUIRED", opts={:type => :string, :short => '-u'})
-      opt(:name, desc="Real name for new TF user - REQUIRED", opts={:type => :string, :short => '-n'})
-      opt(:email, desc="Email address for new TF user - REQUIRED", opts={:type => :string, :short => '-e'})
+      opt(:user_name, desc="Username for new TF user - REQUIRED", opts={:type => :string, :short => '-u'})
+      opt(:full_name, desc="Real name for new TF user - REQUIRED", opts={:type => :string, :short => '-n'})
+      opt(:email_address, desc="Email address for new TF user - REQUIRED", opts={:type => :string, :short => '-e'})
     end
   when "create_vs"
     Trollop::options do
-      opt(:user, desc="TF user to associate this VS with - REQUIRED", opts={:type => :string, :short => '-u'})
-      opt(:name, desc="Descriptive name for new VS - REQUIRED", opts={:type => :string, :short => '-n'})      
+      opt(:user_name, desc="TF user to associate this VS with - REQUIRED", opts={:type => :string, :short => '-u'})
+      opt(:name, desc="Name for new VS - REQUIRED", opts={:type => :string, :short => '-n'})
+      opt(:description, desc="Description of VS's intended use - OPTIONAL", opts={:type => :string, :short => '-d', :default => ''}) 
     end
   when "delete_user"
     Trollop::options do
-      opt(:user, desc="Username of TF user to delete - REQUIRED", opts={:type => :string, :short => '-u'})
+      opt(:user_name, desc="Username of TF user to delete - REQUIRED", opts={:type => :string, :short => '-u'})
       opt(:delete_all, desc="Delete _all_ VSs this user owns", :type => :flag, :default => true)
     end
   when "delete_vs"
     Trollop::options do
-      opt(:user, desc="Username of VS owner - REQUIRED", opts={:type => :string, :short => '-u'})
+      opt(:user_name, desc="Username of VS owner - REQUIRED", opts={:type => :string, :short => '-u'})
       opt(:id, desc="ID of VS to be deleted - REQUIRED", opts={:type => :int, :short => '-i'})
     end
   when nil
