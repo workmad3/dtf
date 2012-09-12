4db99d8250811fe13e33f9c23686a29a84397871
David Deryl Downey <me@daviddwdowney.com>
2012-09-11 22:11:42 -0400

    Changed validation syntax in User model. Added uniqueness check for user_name and email_address

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@
 class User < ActiveRecord::Base
 
   attr_accessible :full_name, :email_address, :user_name
-  validates_presence_of :full_name, :email_address, :user_name
+  validates :full_name, :email_address, :user_name, :presence => true
+  validates :user_name, :email_address, :uniqueness => true
 
   has_many :verification_suites, :dependent => :destroy
   has_many :analysis_cases, :through => :verification_suites, :dependent => :destroy
