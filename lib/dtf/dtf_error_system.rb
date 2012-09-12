204899dd1e0628a3f75bf9a323bbd77f324a3213
David Deryl Downey <me@daviddwdowney.com>
2012-09-12 10:19:43 -0400

    Rewrote raise_error method to print errors to stderr, and to call abort() rather than push exit(err_code)

--- lib/dtf/dtf_error_system.rb
+++ lib/dtf/dtf_error_system.rb
@@ -2,9 +2,14 @@
 def raise_error
   raise ArgumentError
   rescue
-    puts "ERROR! #{@cmd} did not receive all required options."
-    puts "See 'dtf #{@cmd} -h' for help with this sub-command"
+    $stderr.puts "ERROR! #{@cmd} did not receive all required options."
+    $stderr.puts "See 'dtf #{@cmd} -h' for help with this sub-command"
 
     # Set non-zero exit value on error, for scripting use.
-    exit 255
+    abort()
+end
+
+def display_errors()
+  # TODO: Refactor error display to take sub-command as an arg
+  # and display obj.errors.messages.each properly for each arg type.
 end
