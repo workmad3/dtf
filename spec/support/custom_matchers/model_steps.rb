b25b7c9c1eed21b9ac3deb209b6c03ba04d2a379
David Deryl Downey <me@daviddwdowney.com>
2012-09-14 16:19:46 -0400

    Fixed incorrect association build and check in model_steps.rb

--- spec/support/custom_matchers/model_steps.rb
+++ spec/support/custom_matchers/model_steps.rb
@@ -4,27 +4,31 @@ module ModelSteps
 
   step "a user" do
     @user = Fabricate(:user)
+    @user.should be_a(User)
     @user.should_not be_nil
   end
 
   step "a verification suite" do
     @vs = Fabricate(:verification_suite)
+    @vs.should be_a(VerificationSuite)
     @vs.should_not be_nil
   end
 
   step "I should see an analysis case" do
     @ac = Fabricate(:analysis_case)
+    @ac.should be_a(AnalysisCase)
     @ac.should_not be_nil
   end
 
   step "I should see a case test" do
     @ct = Fabricate(:case_test)
+    @ct.should be_a(CaseTest)
     @ct.should_not be_nil
   end
 
   step "I should see user ownership chain via :association" do |association|
-    @user.send(association).build
-    @user.send(association).should_not be_nil
+    @user.send(association.pluralize).build
+    @user.send(association.pluralize).should_not be_empty
   end
 
   step "I create a/an :association" do |association|
