556c91a09e22748fff6f0666061975f4eb9cc52c
David Deryl Downey <me@daviddwdowney.com>
2012-09-14 00:02:30 -0400

    Removed extraneous comments from feature_steps.rb [ci-skip]

--- spec/steps/feature_steps.rb
+++ spec/steps/feature_steps.rb
@@ -4,15 +4,12 @@ step "I have dtf installed" do
   end
 end
 
-# $cmd is any one of the various current dtf cmd management / control structures
-# TODO: Rewrite to use a single dtf control script, and modular cmd|subcmd exec|help systems
 step "I request help for sub-command :sub_cmd" do |cmd|
   @cmds = {}
   @cmds[cmd] = ''
 end
 
 step "I should see :help_response in the response" do |help_response|
-  # Display actual help for specific $cmd space
   @cmds.each do |cmd|
     cmd[1] = "#{help_response}"
     result = %x[bundle exec dtf #{cmd[0]} -h]
